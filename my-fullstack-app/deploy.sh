3a7ab9cc25117fe1bb9f1e706f29115f29c92e0f
rsascha <sascha.rose@gmail.com>
2020-03-06 19:39:05 +0100

    Some renaming; backup

--- my-fullstack-app/deploy.sh
+++ my-fullstack-app/deploy.sh
@@ -2,21 +2,24 @@
 
 MAJOR=0
 MINOR=0
-BUILD=5
+BUILD=14
 
 VERSION=$MAJOR.$MINOR.$BUILD
 
 npm version $VERSION
 
-npm run build
+#npm install
+#npm run build
 
 docker build --tag localhost:32000/api:latest -f k8s.api.Dockerfile .
 docker tag localhost:32000/api:latest localhost:32000/api:$VERSION
 docker push localhost:32000/api:latest
+
+echo docker push localhost:32000/api:$VERSION
 docker push localhost:32000/api:$VERSION
 
 echo sed -i 's/api:.*/api:'$VERSION'/g' k8s.api.deployment.yaml
 sed -i 's/api:.*/api:'$VERSION'/g' k8s.api.deployment.yaml
 
-echo microk8s.kubectl set image deployment/api-deployment api=api:$VERSION --record
-microk8s.kubectl set image deployment/api-deployment api=api:$VERSION --record
+echo microk8s.kubectl set image deployment/api api=api:$VERSION --record
+microk8s.kubectl set image deployment/api api=api:$VERSION --record
